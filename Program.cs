81c9aec73aa60dcad781baa8c51f29e5e9a39bc6
Maryna Bogdan <lagutinam@gmail.com>
2017-01-15 20:27:47 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zvezdochka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Написать программу, которая обрабатывает нажатия 4-ёх клавиш - вверх, вниз, влево, вправо. 
+            //На консоле приблизительно в центре экрана отображается символ звёздочки "*".
+            //По нажатию на каждую из этих клавиш-стрелок, она должна двигаться не оставляя следов.
+            //Звёздочка не должна выходить за края экрана(консоли).                     
+           
+            int x = 50;
+            int y = 12;
+            int flag1 = 119;
+            int flag2 = -1;
+            ConsoleKeyInfo k;
+
+            do
+            {
+                Console.SetCursorPosition(x, y);
+                Console.Write("*");
+                Console.CursorVisible = false;
+                k = Console.ReadKey();
+
+                if (k.Key == ConsoleKey.UpArrow)
+                    y--;
+                else if (k.Key == ConsoleKey.DownArrow)
+                    y++;
+                else if (k.Key == ConsoleKey.LeftArrow)
+                    x--;
+                else if (k.Key == ConsoleKey.RightArrow)
+                    x++;
+                if (x >= flag1)
+                    x--;
+                if (y <= flag2)
+                    y++; 
+                Console.Clear();
+            } while (k.Key != ConsoleKey.Escape);
+
+            
+            Console.ReadKey();
+        }
+    }
+}
